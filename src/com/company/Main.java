5d2d647a70364baaac51811d5b72dc41b5ecd5e0
Scott Stemen <sstemen@kryteriononline.com>
2021-10-27 07:31:54 -0700

    Second Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Scott");
+        System.out.println("Text can be fun ...");
+        int foo = 5;
+        System.out.println("Multiplication " + (foo * foo));
+    }
+}
